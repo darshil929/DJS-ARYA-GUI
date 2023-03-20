06236520c92064bed56812c99fcbb4849f3e7baa
Kimaya Ved <102381197+kimayav@users.noreply.github.com>
2023-03-21 00:53:43 +0530

    start stop int

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,6 @@
 const socket = io('http://localhost:3000')
- socket.on('connection',() => {
-
+socket.on('connection', () => {
 })
-
 var voltageData = [];
 var temperatureData = [];
 var altitudeData = [];
@@ -13,8 +11,6 @@ var pressureData = [];
 var gpsAltitudeData = [];
 var gpsLatitudeData = [];
 var gpsLongitudeData = [];
-
-
 const Time = document.querySelector('.m-time');
 const State = document.querySelector('.state');
 const packet_count = document.querySelector('.packetCount');
@@ -32,18 +28,17 @@ const pressure = document.querySelector('.pressure');
 // let threeD;
 // const threeData= new vis.DataSet();
 // let visFlag = 0;
-
 //3D Chart
-    // let threeD;
-    // const threeData = new vis.DataSet();
-    // let visFlag = 0;
-
+// let threeD;
+// const threeData = new vis.DataSet();
+// let visFlag = 0;
 // 3d chart
 let threeD;
 const threeData = new vis.DataSet();
 let visFlag = 0;
 
-socket.on('trial.csv', (myData) => {
+socket.on('data', (myData) => {
+    console.log(myData)
     voltageData.push(myData.voltage);
     temperatureData.push(myData.temperature);
     altitudeData.push(myData.altitude);
@@ -54,14 +49,12 @@ socket.on('trial.csv', (myData) => {
     gpsAltitudeData.push(myData.gpsAltitude);
     gpsLatitudeData.push(myData.gpsLatitude);
     gpsLongitudeData.push(myData.gpsLongitude);
-
     dynamicChart1.update();
     dynamicChart2.update();
     dynamicChart3.update();
     dynamicChart4.update();
     dynamicChart5.update();
 
-    
     Time.innerHTML = `${myData.missionTime}`;
     State.innerHTML = `${myData.state}`;
     packet_count.innerHTML = `${myData.packetCount}`;
@@ -78,35 +71,35 @@ socket.on('trial.csv', (myData) => {
     pressure.innerHTML = `${myData.pressure}`;
 
     createContainerTable(myData);
-    // transition_ss(myData.state);
-    // displayStates(myData.state);
+    transition_ss(myData.state);
+    displayStates(myData.state);
+    randomState(myData.state);
+    displayStates(myData.state);
     create3dChart(myData);
 
     // MAP
-        // // marker.setLatLng([myData.gpsLatitude, myData.gpsLongitude]);
-        // let r1 = myData.gpsLatitude;
-        // let r2 = myData.gpsLongitude;
-        // r1 = r1.toString();
-        // r2 = r2.toString();
-        // r1 = parseFloat( '0.0' + r1.substring(5,8));
-        // r2 = parseFloat('0.0' + r2.substring(5,8));
-        // // marker.setLatLng([19.076 + r1, 72.877 + r2]);
-        marker.setLatLng([myData.gpsLatitude, myData.gpsLongitude]);
-
-        // const circle = L.circle([myData.gpsLatitude, myData.gpsLongitude], {   // marking the past Lat Long
-        // const circle = L.circle([19.076 + r1, 72.877 + r2], {   // marking the past Lat Long
-        const circle = L.circle([myData.gpsLatitude, myData.gpsLongitude], {   // marking the past Lat Long
-            color: "blue",
-            fillColor: "blue",
-            fillOpacity: 1,
-            radius: 1.0
-        }).addTo(map);
+    // // marker.setLatLng([myData.gpsLatitude, myData.gpsLongitude]);
+    // let r1 = myData.gpsLatitude;
+    // let r2 = myData.gpsLongitude;
+    // r1 = r1.toString();
+    // r2 = r2.toString();
+    // r1 = parseFloat( '0.0' + r1.substring(5,8));
+    // r2 = parseFloat('0.0' + r2.substring(5,8));
+    // // marker.setLatLng([19.076 + r1, 72.877 + r2]);
+    marker.setLatLng([myData.gpsLatitude, myData.gpsLongitude]);
+    // const circle = L.circle([myData.gpsLatitude, myData.gpsLongitude], {   // marking the past Lat Long
+    // const circle = L.circle([19.076 + r1, 72.877 + r2], {   // marking the past Lat Long
+    const circle = L.circle([myData.gpsLatitude, myData.gpsLongitude], {   // marking the past Lat Long
+        color: "blue",
+        fillColor: "blue",
+        fillOpacity: 1,
+        radius: 1.0
+    }).addTo(map);
 })
 
-// Tab Switch
+    // Tab Switch
     const tabs = document.querySelectorAll('[data-tab-target]')
     const tabContents = document.querySelectorAll('[data-tab-content]')
-
     tabs.forEach(tab => {
         tab.addEventListener('click', () => {
             setTimeout(function () { map.invalidateSize() }, 1)
@@ -121,17 +114,20 @@ socket.on('trial.csv', (myData) => {
             target.classList.add('active')
         })
     })
-
-//BUTTON INTEGRATION
-// let startFlag=0;
-// let stopFlag=0;
-document.querySelector('#btn1').addEventListener('click', async () => {
-    socket.connect()
+    //BUTTON INTEGRATION
+    // let startFlag=0;
+    // let stopFlag=0;
+    document.querySelector('#btn1').addEventListener('click', async () => {
+        // socket.connect()
+        socket.emit('click', 'start');
+        console.log()
+        // socket.connect()
         //// await socket.emit('start');
         //// writeOnSerial('1');  
         //startFlag = 1;  
-})
-document.querySelector('#btn2').addEventListener('click', async () => {
+    })
+    document.querySelector('#btn2').addEventListener('click', async () => {
         socket.disconnect();
-    }
-)
+        socket.emit('click', 'stop');
+        // socket.disconnect();
+    })
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// Run the GUI on Server
-
 const fs = require('fs'); 
 const express = require('express');
 const app = express();
@@ -7,13 +5,16 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: "*"} })
 const path = require('path');
 const makeCSV = require('./utils/makeCSV.js');
+const parse = require('./utils/parse.js');
 
 app.use(express.static("public"));
 
 server.listen(3000,() => {
     console.log(`Server is UP on port 3000`);
 })
+
 let data;
+
 try {
     const bufferData = fs.readFileSync('./test.csv')
     data = bufferData.toString();
@@ -23,12 +24,6 @@ try {
 
 data = data.split('\r\n');
 data.shift();
-
-// const io = new Server(server, {
-//     cors: {
-//         origin: "http://localhost:3000",
-//     }
-// });
 
 io.on('connection', (socket) => {
     console.log(`Connected: ${socket.id}`);
@@ -43,29 +38,9 @@ io.on('connection', (socket) => {
                     clearInterval(interval);
                 }
 
+                makeCSV(data[cntr]);
                 const row = data[cntr].split(',');
-                const obj = {
-                    teamId : row[0],
-                    missionTime : row[1],
-                    packetCount : Number(row[2]),
-                    mode : row[3],
-                    state : Number(row[4]),
-                    altitude : Number(row[5]).toFixed(1),
-                    hsDeployed : row[6],
-                    pcDeployed : row[7],
-                    mastRaised : row[8],
-                    temperature : Number(row[9]).toFixed(1),
-                    voltage : Number(row[10]).toFixed(1),
-                    pressure : Number(row[11]).toFixed(1),
-                    gpsTime : row[12],
-                    gpsAltitude : Number(row[13]).toFixed(1),
-                    gpsLatitude : Number(row[14]).toFixed(4),
-                    gpsLongitude : Number(row[15]).toFixed(4),
-                    gpsSats : Number(row[16]),
-                    tiltX : Number(row[17]).toFixed(2),
-                    tiltY : Number(row[18]).toFixed(2),        
-                    cmdEcho : row[19]
-                }
+                const obj = parse(row);
 
                 socket.emit('data', obj);
             }, 1000);
